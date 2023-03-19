4ea2cee47facb922843aafaad76a07568cdeaac1
Eliel-Denisovas <107564817+Eliel-Denisovas@users.noreply.github.com>
2023-03-19 17:43:05 +0000

    Update

---
+++ front_end/test/teste.js
@@ -0,0 +1,9 @@
+const node_list = document.querySelectorAll('ul li');
+const collection_list = document.getElementById('ul').getElementsByTagName('li');
+const h1 = document.querySelector('h1');
+const ul = document.getElementById('ul');
+const a = document.querySelector('a');
+
+console.log(a);
+console.log(a.parentElement.lastElementChild);
+console.log(a.lastChild);
