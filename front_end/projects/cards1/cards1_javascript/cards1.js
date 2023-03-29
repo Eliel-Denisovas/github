5026c019bb86570c24894394452888aefd185df0
Eliel-Denisovas <107564817+Eliel-Denisovas@users.noreply.github.com>
2023-03-29 22:04:04 +0100

    project cards1 update

--- front_end/projects/cards1/cards1_javascript/cards1.js
+++ front_end/projects/cards1/cards1_javascript/cards1.js
@@ -1,13 +1,113 @@
 // Efects
 // Error red light
-const cardLeft = document.getElementById('cardLeft');
+const cardLeft = document.getElementById("cardLeft");
+const cardRight = document.getElementById("cardRight");
+const roundDisplay = document.getElementById("numberFraction");
+const luckDisplay = document.getElementById("divisorFraction");
+const moneyDisplay = document.getElementById("numberPercentual");
 
-cardLeft.addEventListener('click', function onClick(event) {
-  const box = document.getElementById('containerCards');
-});
+let userCard = undefined;
+let computerCard = undefined;
+let roundStatus = true;
+
+let currentRound = 1;
+let rounds = 0;
+const roundValue = currentMoney / rounds;
+
+let luck = 0.5;
+const arrayLuck = [];
+
+let currentMoney = 0;
+
+fun
+
+function checkCards(userCard, computerCard) {
+  if (userCard === computerCard) {
+    roundStatus = true;
+  } else {
+    roundStatus = false;
+  };
+
+};
+
+function checkRound () {
+  roundValue += 1
+};
+
+/* function checkLuck () {
+  arrayLuck.filter(function(currentValue, index, arr) {
+
+  }
+
+  
+
+}; */
+
+function checkMoney(roundStatus) {
+  if (roundStatus) {
+    currentMoney += roundValue;
+  } else {
+    currentMoney -= roundValue;
+  };
+};
 
 function correctChoice() {
-  let element = document.getElementById('containerCards');
+  let element = document.getElementById("containerCards");
   element.classList.add("correctChoice");
-  cardLeft.classList.add("glassHoverReverse");
+  setTimeout(glassNormalSize, 0);
+};
+
+function wrongChoice() {
+  let element = document.getElementById("containerCards");
+  element.classList.add("wrongChoice");
+  setTimeout(glassNormalSize, 0);
+};
+
+function glassNormalSize() {
+  let element = document.getElementById("cardLeft");
+  element.classList.remove("glassHover");
+  let element2 = document.getElementById("cardRight");
+  element2.classList.remove("glassHover");
+};
+
+function table1Efect (){
+  if (roundStatus) {
+    correctChoice();
+    glassNormalSize();
+  } else {
+    wrongChoice();
+    glassNormalSize();
+  };
 }
+
+function insertData() {
+  roundDisplay.innerText = currentRound;
+  luckDisplay.innerText = luck;
+  moneyDisplay.innerText = currentMoney;
+};
+
+function roundFunctions () {
+  checkCards();
+  checkMoney();
+  checkLuck();
+
+};
+
+const ages = [32, 33, 12, 40];
+
+function checkAge(age) {
+  return age > document.getElementById("ageToCheck").value;
+}
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = ages.filter(checkAge);
+}
+
+cardLeft.addEventListener('click', function() {
+  userCard = 0;
+
+});
+
+cardRight.addEventListener('click', function() {
+  userCard = 1;
+});
