11e380f1d1fa2c6d15c1c6f28bb225ec4bf26952
Eliel-Denisovas <107564817+Eliel-Denisovas@users.noreply.github.com>
2023-04-16 13:44:17 +0100

    project cards1 update 16-4-23

--- front_end/projects/cards1/cards1_javascript/cards1.js
+++ front_end/projects/cards1/cards1_javascript/cards1.js
@@ -11,7 +11,6 @@ const moneyDisplay = document.getElementById("money");
 const previousStatusDisplay = document.getElementById("previousStatus");
 let PointsDisplayContainer = document.getElementById("PointsDisplayContainer");
 
-
 //const startButton = document.getElementById("startButton");
 
 /* 
@@ -24,9 +23,7 @@ numberFractionDisplay.textContent = (currentRound + 1).toFixed(0);
 
 let computerCardsArray = [];
 
-let startMoney = parseFloat(
-  prompt("Type a value", "")
-);
+let startMoney = parseFloat(prompt("Type a value", ""));
 let currentMoney = parseFloat(startMoney);
 let rounds = parseInt(
   prompt("TYPE HOW MANY ROUDS YOU WANT TO PLAY IN THIS MATCH", "")
@@ -50,8 +47,9 @@ let luck = 0.5;
   }
 })();
 
-document.querySelector(".page").classList.remove("disableElement");
+console.log(computerCardsArray);
 
+document.querySelector(".page").classList.remove("disableElement");
 
 function addCheckPoint() {
   const checkPoint = document.createElement("div");
@@ -64,7 +62,9 @@ for (let i = 0; i < rounds; i++) {
   addCheckPoint();
 }
 
-PointsDisplayContainer.getElementsByClassName("checkPoint")[0].classList.add("choice");
+PointsDisplayContainer.getElementsByClassName("checkPoint")[0].classList.add(
+  "choice"
+);
 
 function checkCards(userCard, computerCard) {
   if (userCard === computerCard) {
@@ -96,9 +96,23 @@ function checkMoney(roundStatus) {
 }
 
 function insertData() {
-  setTimeout(() => {
-    numberFractionDisplay.textContent = (currentRound + 1).toFixed(0);
-  }, 1000);
+  if (currentRound > 0) {
+    setTimeout(() => {
+      document.getElementById("roundSound").play();
+      document.getElementById("roundSound").volume = 0.5;
+
+      document.getElementById("numberFraction").classList.add("textScaleUp");
+      document.getElementById("numberFraction").classList.add("shadowText2");
+      setTimeout(() => {
+        document.getElementById("numberFraction").classList.remove("textScaleUp");
+        document.getElementById("numberFraction").classList.remove("shadowText2");
+      }, 1001);
+
+      document.getElementById("numberFraction").classList.add("textScaleUp")
+      numberFractionDisplay.textContent = (currentRound + 1).toFixed(0);
+    }, 1000);
+  }
+
   divisorFractionDisplay.textContent = rounds.toFixed(0);
   numberPercentualDisplay.textContent = luck.toFixed(2);
   moneyDisplay.textContent = currentMoney.toFixed(2);
@@ -163,7 +177,6 @@ function cleanClasses() {
     .classList.remove("rejectedCardRightTurn2");
 
   document.getElementById("containerCards").classList.add("glass");
-
   document.getElementById("cardRight").classList.add("glass");
   document.getElementById("cardLeft").classList.add("glass");
 
@@ -195,26 +208,35 @@ function ableCards() {
 }
 
 function setCheckPoints() {
-  let lastCheckPoint = currentRound-1;
-  
-  PointsDisplayContainer.getElementsByClassName("checkPoint")[
-    lastCheckPoint
-  ].classList.remove("choice");
-  PointsDisplayContainer.getElementsByClassName("checkPoint")[
-    currentRound
-  ].classList.remove("choice");
-  PointsDisplayContainer.getElementsByClassName("checkPoint")[
-    lastCheckPoint+1
-  ].classList.add("choice");
+  let checkPointsValue = currentRound;
+  let functionTime = 3800;
+
+  setTimeout(() => {
+    PointsDisplayContainer.getElementsByClassName("checkPoint")[
+      checkPointsValue
+    ].classList.remove("choice");
+  }, functionTime);
 
   if (roundStatus) {
-    PointsDisplayContainer.getElementsByClassName("checkPoint")[
-      lastCheckPoint
-    ].classList.add("greenLightBox");
+    setTimeout(() => {
+      PointsDisplayContainer.getElementsByClassName("checkPoint")[
+        checkPointsValue
+      ].classList.add("greenLightBox");
+    }, functionTime);
   } else {
-    PointsDisplayContainer.getElementsByClassName("checkPoint")[
-      lastCheckPoint
-    ].classList.add("redLightBox");
+    setTimeout(() => {
+      PointsDisplayContainer.getElementsByClassName("checkPoint")[
+        checkPointsValue
+      ].classList.add("redLightBox");
+    }, functionTime);
+  }
+
+  if (checkPointsValue <= rounds - 2) {
+    setTimeout(() => {
+      PointsDisplayContainer.getElementsByClassName("checkPoint")[
+        checkPointsValue + 1
+      ].classList.add("choice");
+    }, functionTime);
   }
 }
 
@@ -229,6 +251,7 @@ function roundFunctions() {
     setTimeout(() => {
       insertData();
     }, 3000);
+    setCheckPoints();
     return;
   } else {
     computerCard = computerCardsArray[currentRound];
@@ -239,17 +262,16 @@ function roundFunctions() {
     setTimeout(() => {
       insertData();
     }, 3000);
+    setCheckPoints();
     setTimeout(() => {
       ableCards();
     }, 5000);
-    setTimeout(() => {
-      setCheckPoints();
-    }, 4000);
     currentRound += 1;
   }
 }
 
 function cardLeftEfect() {
+  document.getElementById("choiceSound").play();
   rejectedCardRight();
   acceptedCardLeft();
 
@@ -284,20 +306,33 @@ function cardLeftEfect() {
     document
       .getElementById("cardRight")
       .classList.add("rejectedCardRightTurn2");
-  }, 1350);
+  }, 1500);
 
   setTimeout(() => {
     document.getElementById("containerCards").classList.remove("choice");
 
     if (roundStatus) {
       document.getElementById("containerCards").classList.add("correctChoice");
+      document.getElementById("choiceSound").pause();
+      document.getElementById("correctChoiceSound").play();
+
+      document.getElementById("luckWord").classList.add("textScaleUp");
+      document.getElementById("luckWord").classList.add("greenLightText");
+      setTimeout(() => {
+        document.getElementById("luckWord").classList.remove("textScaleUp");
+        document.getElementById("luckWord").classList.remove("greenLightText");
+      }, 3000);
+
     } else {
       document.getElementById("containerCards").classList.add("wrongChoice");
+      document.getElementById("choiceSound").pause();
+      document.getElementById("wrongChoiceSound").play();
     }
   }, 1500);
 }
 
 function cardRightEfect() {
+  document.getElementById("choiceSound").play();
   rejectedCardLeft();
   acceptedCardRight();
 
@@ -332,15 +367,25 @@ function cardRightEfect() {
     }
 
     document.getElementById("cardLeft").classList.add("rejectedCardLeftTurn2");
-  }, 1350);
+  }, 1500);
 
   setTimeout(() => {
     document.getElementById("containerCards").classList.remove("choice");
 
     if (roundStatus) {
       document.getElementById("containerCards").classList.add("correctChoice");
+      document.getElementById("choiceSound").pause();
+      document.getElementById("correctChoiceSound").play();
+      document.getElementById("luckWord").classList.add("textScaleUp");
+      document.getElementById("luckWord").classList.add("greenLightText");
+      setTimeout(() => {
+        document.getElementById("luckWord").classList.remove("textScaleUp");
+        document.getElementById("luckWord").classList.remove("greenLightText");
+      }, 1001);
     } else {
       document.getElementById("containerCards").classList.add("wrongChoice");
+      document.getElementById("choiceSound").pause();
+      document.getElementById("wrongChoiceSound").play();
     }
   }, 1500);
 }
@@ -351,6 +396,9 @@ cardLeft.addEventListener("click", function () {
   roundFunctions();
   cardLeftEfect();
   setTimeout(() => {
+    document.getElementById("choiceSound").play();
+  }, 2500);
+  setTimeout(() => {
     cleanClasses();
   }, 3000);
 });
@@ -360,6 +408,9 @@ cardRight.addEventListener("click", function () {
   userCardsArray.push(userCard);
   roundFunctions();
   cardRightEfect();
+  setTimeout(() => {
+    document.getElementById("choiceSound").play();
+  }, 2500);
   setTimeout(() => {
     cleanClasses();
   }, 3000);
